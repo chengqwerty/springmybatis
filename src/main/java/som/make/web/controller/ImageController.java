820143158b7d995f89fce4671db8a185545760fe
chengqwerty <shmily_think@126.com>
2017-10-19 17:16:12 +0800

    2017-10-09 17:16

--- src/main/java/som/make/web/controller/ImageController.java
+++ src/main/java/som/make/web/controller/ImageController.java
@@ -1,6 +1,7 @@
 package som.make.web.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.ServletOutputStream;
@@ -12,22 +13,31 @@ import java.io.*;
 @RequestMapping("/image")
 public class ImageController {
 
+    @CrossOrigin
+    @RequestMapping
+    public String getImageUrl() {
+        return null;
+    }
+
+    /**
+     * @CrossOrigin注解用来处理跨域
+     * @param httpServletRequest
+     * @param httpServletResponse
+     * @throws IOException
+     */
+    @CrossOrigin()
     @RequestMapping("/easy")
     public void getEasyImage(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws IOException {
-        System.out.println("=======================");
         String path = httpServletRequest.getServletContext().getRealPath("image/meinv.jpg");
-        System.out.println(path);
         File file = new File(path);
-        System.out.println("file exists is " + file.exists());
         FileInputStream fileInputStream = new FileInputStream(file);
-        //FileOutputStream fileOutputStream = new FileOutputStream(new File(path));
         int allLength = fileInputStream.available();
         byte[] bytes = new byte[allLength];
         fileInputStream.read(bytes, 0, bytes.length);
-        System.out.println("allLength is " + allLength);
         ServletOutputStream servletOutputStream = httpServletResponse.getOutputStream();
         httpServletResponse.setHeader("Content-Type","image/jped");
         httpServletResponse.setHeader("content-disposition", "attachment;filename=abv.jpg");
+        //httpServletResponse.setHeader("Access-Control-Allow-Origin", "http://localhost:8080");
         servletOutputStream.write(bytes);
         servletOutputStream.close();
     }
